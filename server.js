895bae9f86e4ffe22662449d4535b08e7642eba0
BROLAK <matiborlak@gmail.com>
2023-02-24 20:32:55 +0000

    middleware

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const session = require("express-session");
 const passport = require("passport");
 const { ObjectID } = require("mongodb");
+const LocalStrategy = require("passport-local");
 
 const app = express();
 
@@ -29,6 +30,13 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
+
 myDB(async (client) => {
   const myDataBase = await client.db("database").collection("users");
 
@@ -36,8 +44,34 @@ myDB(async (client) => {
     res.render("index", {
       title: "Connected to Database",
       message: "Please log in",
+      showLogin: true,
     });
   });
+
+  app
+    .route("/login")
+    .post(
+      passport.authenticate("local", { failureRedirect: "/" }),
+      (req, res) => {
+        res.redirect("/profile");
+      }
+    );
+
+  app.route("/profile").get(ensureAuthenticated, (req, res) => {
+    res.render("profile");
+  });
+
+  passport.use(
+    new LocalStrategy((username, password, done) => {
+      myDataBase.findOne({ username: username }, (err, user) => {
+        console.log(`User ${username} attempted to log in.`);
+        if (err) return done(err);
+        if (!user) return done(null, false);
+        if (password !== user.password) return done(null, false);
+        return done(null, user);
+      });
+    })
+  );
 
   passport.serializeUser((user, done) => {
     done(null, user._id);
@@ -50,7 +84,10 @@ myDB(async (client) => {
   });
 }).catch((e) => {
   app.route("/").get((req, res) => {
-    res.render("index", { title: e, message: "Unable to connect to database" });
+    res.render("index", {
+      title: e,
+      message: "Unable to connect to database",
+    });
   });
 });
 
