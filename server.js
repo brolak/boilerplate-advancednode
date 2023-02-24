eb3fd56a0425d3553669059a6bfc2914739c090f
BROLAK <matiborlak@gmail.com>
2023-02-24 20:20:36 +0000

    connect mongo

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 "use strict";
 require("dotenv").config();
 const express = require("express");
-const session = require("express-session");
 const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const session = require("express-session");
 const passport = require("passport");
+const { ObjectID } = require("mongodb");
+
 const app = express();
 
 app.set("view engine", "pug");
@@ -22,16 +24,37 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-fccTesting(app); //For FCC testing purposes
+fccTesting(app); // For fCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route("/").get((req, res) => {
-  res.render("index", { title: "Hello", message: "Please log in" });
+myDB(async (client) => {
+  const myDataBase = await client.db("database").collection("users");
+
+  app.route("/").get((req, res) => {
+    res.render("index", {
+      title: "Connected to Database",
+      message: "Please log in",
+    });
+  });
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+}).catch((e) => {
+  app.route("/").get((req, res) => {
+    res.render("index", { title: e, message: "Unable to connect to database" });
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Listening on port " + PORT);
+  console.log(`Listening on port ${PORT}`);
 });
