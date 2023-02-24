e1d1a98249541d3deaab85e77f2734b4fff4e024
BROLAK <matiborlak@gmail.com>
2023-02-24 19:53:55 +0000

    passport init

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 "use strict";
 require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
 const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
-
+const passport = require("passport");
 const app = express();
+
 app.set("view engine", "pug");
 app.set("views", "./views/pug");
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
