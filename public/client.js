729ffd62c94640a12784228d19c72f36cfcd4903
BROLAK <matiborlak@gmail.com>
2023-03-15 11:51:24 +0000

    comm emit

--- public/client.js
+++ public/client.js
@@ -2,24 +2,14 @@ $(document).ready(function () {
   /*global io*/
   let socket = io();
 
-  socket.on("user", (data) => {
-    $("#num-users").text(data.currentUsers + " users online");
-    let message =
-      data.username +
-      (data.connected ? " has joined the chat." : " has left the chat.");
-    $("#messages").append($("<li>").html("<b>" + message + "</b>"));
-  });
-
-  socket.on("chat message", (data) => {
-    console.log("socket.on 1");
-    $("#messages").append($("<li>").text(`${data.username}: ${data.message}`));
+  socket.on("user count", function (data) {
+    console.log(data);
   });
 
   // Form submittion with new message in field with id 'm'
   $("form").submit(function () {
-    let messageToSend = $("#m").val();
-    //send message to server here?
-    socket.emit("chat message", messageToSend);
+    var messageToSend = $("#m").val();
+
     $("#m").val("");
     return false; // prevent form submit from refreshing page
   });
